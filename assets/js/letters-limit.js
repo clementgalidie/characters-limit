09dc64f2b320df58d2d2ee3f7c4e79008e7c7129
Clément <galidie.clement@gmail.com>
2015-01-09 19:00:25 +0100

    First commit: initialize readme, assets and html file

---
+++ assets/js/letters-limit.js
@@ -0,0 +1,10 @@
+(function () {
+	"use strict";
+
+	var textArea = document.getElementById("message");
+
+	// Function to count the number of letters.
+	var countLetters = function (e) {
+		
+	}
+}());
