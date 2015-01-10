4f9c3e0449676c791fcd2fb0a53d388f7d98901c
Clement Galidie <galidie.clement@gmail.com>
2015-01-10 22:45:17 +0100

    Update readme, index file and assets

--- assets/js/letters-limit.js
+++ assets/js/letters-limit.js
@@ -2,9 +2,21 @@
 	"use strict";
 
 	var textArea = document.getElementById("message");
+	var textAreaCounter = document.querySelector(".message-lettersCounter");
+	var maxLettersNumber = "150" // Set the maximum number of letters user can write.
 
 	// Function to count the number of letters.
-	var countLetters = function (e) {
-		
+	var countLetters = function () {
+		maxLettersNumber--;
+		textAreaCounter.textContent = maxLettersNumber;
+
+		// If the user has written more than 150 characters, then disable textarea.
+		if (maxLettersNumber < 0) {
+			textArea.setAttribute("disabled", "disabled");
+			textAreaCounter.textContent = 0;
+		}
 	}
+
+	// When someone is writting something in the textarea, call countLetters().
+	textArea.addEventListener("keydown", countLetters);
 }());
